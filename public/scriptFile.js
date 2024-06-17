3e6e6730040c3652f7a869f5972d9b83af98eded
Shihab AbdalRahman <shihab.abdulrahman@jcbc.local>
2024-06-17 13:06:42 +0300

    v1

---
+++ public/scriptFile.js
@@ -0,0 +1,7 @@
+let h1Tag = document.getElementById('h1');
+let buttonTag = document.getElementById('btn');
+
+
+buttonTag.addEventListener('click',function(){
+    h1Tag.innerHTML= 'welcome to course';
+})
